695a3166d81f338df0e59447fdb755e19a3b516f
GD <GD@DESKTOP-FM04MHN>
2025-11-26 15:29:52 +0900

    [정순오] 학과 게시판 service, mapper, view 작업중

---
+++ lms/src/main/java/com/example/lms/service/BoardDeptService.java
@@ -0,0 +1,32 @@
+package com.example.lms.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.lms.dto.BoardDepartment;
+import com.example.lms.mapper.BoardDeptMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+public class BoardDeptService {
+	@Autowired
+	BoardDeptMapper boardDeptMapper;
+	
+	public List<BoardDepartment> getDeptBoardList(
+			@Param("currentPage") int currentPage
+			,@Param("rowPerPage") int rowPerPage
+			,@Param("departmentId") int departmentId) {
+				
+		int startRow = (currentPage-1)*rowPerPage;								 
+						
+		return boardDeptMapper.selectBoardDeptListRowPerPage(departmentId, startRow, rowPerPage);
+	}
+}
